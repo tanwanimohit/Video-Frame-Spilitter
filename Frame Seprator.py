1676528804646179614c886abf69988067338b28
Mohit Tanwani <mohitmurlidhar@gmail.com>
2019-07-04 10:58:59 +0530

    Initial Commit

---
+++ Frame Seprator.py
@@ -0,0 +1,38 @@
+#Open CV for Video Parsing
+import cv2
+# import the os module  for folder creation
+import os
+
+# define the name of the directory to be created
+pwd=os.getcwd();
+path = pwd+"/frames"
+
+try:  
+    os.mkdir(path)
+except OSError:  
+    print ("Maybe Frames Folder Already Exists if not Create it manually than run the Program")
+else:  
+    print ("Successfully created the directory %s " % path)
+
+#Video Location
+string = raw_input("Enter the video location or URL : ")
+vidcap = cv2.VideoCapture(string)
+image = vidcap.read()
+
+#To Maintain the Frames Count
+count = 0
+
+success=True
+print("Spliting Video Frames")
+
+#Main Logic
+while success:
+  success,image = vidcap.read()
+  if(success):
+      cv2.imwrite("frames/frame%d.jpg" % count, image)
+      count += 1 
+else:
+  pass
+
+print("Done!")
+print "Total ",count-1," images Generated "
